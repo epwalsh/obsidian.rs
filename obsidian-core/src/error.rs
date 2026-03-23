7ad3c238ac537c2bc0de836f3cc4410aa8bd8444
epwalsh <epwalsh10@gmail.com>
2026-03-22 19:51:07 -0700

    error clean up

--- obsidian-core/src/error.rs
+++ obsidian-core/src/error.rs
@@ -6,7 +6,7 @@ pub enum VaultError {
     NotADirectory(PathBuf),
     #[error("note already exists at '{0}'")]
     NoteAlreadyExists(PathBuf),
-    #[error("note not found '{0}'")]
+    #[error("note not found: '{0}'")]
     NoteNotFound(String),
     #[error("ambigous note identifier '{0}'; multiple matches found")]
     AmbiguousNoteIdentifier(String, Vec<PathBuf>),
@@ -34,14 +34,14 @@ pub enum NoteError {
     Yaml(#[from] serde_yaml::Error),
     #[error("note content not loaded; use from_path_with_content() or load_content()")]
     ContentNotLoaded,
-    #[error("{0} is not a valid note path")]
+    #[error("'{0}' is not a valid note path")]
     InvalidPath(PathBuf),
 }
 
 #[derive(Debug, thiserror::Error)]
 pub enum SearchError {
-    #[error("invalid glob pattern: {0}")]
+    #[error("invalid glob pattern: '{0}'")]
     InvalidGlob(#[from] globset::Error),
-    #[error("invalid regex pattern: {0}")]
+    #[error("invalid regex pattern: '{0}'")]
     InvalidRegex(#[from] regex::Error),
 }
